47d03406664e87b0dcc2077d094fd97ff41ff66b
vinay77175 <vinay.rockymountaintech@gmail.com>
2024-02-02 17:35:40 +0530

    home_background

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        href="https://fonts.googleapis.com/css?family=Jacques Francois"
+        rel="stylesheet"
+      ></link>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
