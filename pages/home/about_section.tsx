16d040414c753ebd0b99796d545c2bf0255e7b6d
Arham jain <jainarham2101@gmail.com>
2024-02-05 16:39:26 +0530

    About us Section

--- pages/home/about_section.tsx
+++ pages/home/about_section.tsx
@@ -1,16 +1,221 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 import React from "react";
 import backImg1 from "../../public/Images/about_back.png";
+import backImg2 from "../../public/Images/about_image_1.png";
+import backImg3 from "../../public/Images/about_image_2.png";
+import backImg4 from "../../public/Images/about_image_4.png";
 const About_section = () => {
   return (
     <Box
       sx={{
-        minHeight: "100vh",
+        // minHeight: "100vh",
+        // width: "100%",
         bgcolor: "#B12930",
         backgroundImage: `url(${backImg1.src})`,
-        backgroundSize:"cover"
+        backgroundSize: "cover",
+        alignItems: "center",
+        display: "flex",
       }}
-    ></Box>
+    >
+      <Grid container sx={{ display: "flex" }}>
+        <Grid
+          item
+          sx={{
+            alignItems: "center",
+            display: "flex",
+            padding: "30px 0px 0px 40px",
+            position: "relative",
+          }}
+          sm={12}
+          md={12}
+          lg={7}
+        >
+          <img
+            style={{
+              width: "20%",
+              position: "absolute",
+              top: "6vw",
+              right: "12vw",
+            }}
+            src={backImg4.src}
+            alt=""
+          />
+          <img
+            style={{ width: "60%", zIndex: "3", border: "20px white solid" }}
+            src={backImg2.src}
+            alt="image_1"
+          />
+          <img
+            style={{
+              width: "40%",
+              marginLeft: "-7vw",
+              zIndex: "5",
+              border: "20px white solid",
+            }}
+            src={backImg3.src}
+            alt="image_1"
+          />
+          <img
+            style={{
+              width: "20%",
+              position: "absolute",
+              bottom: "4vw",
+              right: "12vw",
+              transform: "scaleY(-1)",
+            }}
+            src={backImg4.src}
+            alt=""
+          />
+        </Grid>
+        <Grid item sm={12} md={12} lg={5}>
+          <Typography
+            variant="h2"
+            sx={{
+              fontFamily: "Jacques Francois",
+              color: "white",
+              marginTop: { xs: "15vh", lg: "5vh" },
+              textAlign: { xs: "center", lg: "left" },
+            }}
+          >
+            About Us
+          </Typography>
+
+          <Typography
+            variant="h3"
+            sx={{
+              marginTop: { xs: "15vh", lg: "15vh" },
+              textAlign: { xs: "center", lg: "left" },
+              fontFamily: "Jacques Francois",
+              color: "white",
+            }}
+          >
+            Sit amet, consectetur adipiscing elit.
+          </Typography>
+
+          <Typography
+            sx={{
+              marginTop: { xs: "8vh", lg: "5vh " },
+              fontFamily: "Times New Roman",
+              color: "white",
+              marginX: { xs: "20vh", lg: "0vh" },
+              textAlign: { xs: "center", lg: "left" },
+            }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Curabitur consequat. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Curabitur consequat.
+          </Typography>
+
+          <Box
+            sx={{
+              display: "flex",
+              paddingTop: "5vh ",
+              paddingX: { xs: "10vh", lg: "0vh" },
+            }}
+          >
+            <Grid
+              container
+              sx={{ justifyContent: { xs: "space-around", lg: "normal" } }}
+            >
+              <Grid item>
+                <Typography
+                  sx={{
+                    fontFamily: "Times New Roman ",
+                    color: "white",
+                    fontSize: "18px",
+                    marginBottom: "12px",
+                    paddingX: { xs: "3vh", lg: "0vh" },
+                  }}
+                >
+                  Sit amet, consectetur adipiscing elit.
+                </Typography>
+                <Typography
+                  sx={{
+                    fontFamily: "Times New Roman ",
+                    color: "white",
+                    fontSize: "18px",
+                    marginBottom: "12px",
+                    paddingX: { xs: "3vh", lg: "0vh" },
+                  }}
+                >
+                  Sit amet, consectetur adipiscing elit.
+                </Typography>
+                <Typography
+                  sx={{
+                    fontFamily: "Times New Roman ",
+                    color: "white",
+                    fontSize: "18px",
+                    paddingX: { xs: "3vh", lg: "0vh" },
+                  }}
+                >
+                  Sit amet, consectetur adipiscing elit.
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              sx={{ justifyContent: { xs: "space-around", lg: "normal" } }}
+            >
+              <Grid item>
+                <Typography
+                  sx={{
+                    fontFamily: "Times New Roman ",
+                    color: "white",
+                    fontSize: "18px",
+                    marginBottom: "12px",
+                    paddingX: { xs: "3vh", lg: "0vh" },
+                  }}
+                >
+                  Sit amet, consectetur adipiscing elit.
+                </Typography>
+                <Typography
+                  sx={{
+                    fontFamily: "Times New Roman ",
+                    color: "white",
+                    fontSize: "18px",
+                    marginBottom: "12px",
+                    paddingX: { xs: "3vh", lg: "0vh" },
+                  }}
+                >
+                  Sit amet, consectetur adipiscing elit.
+                </Typography>
+                <Typography
+                  sx={{
+                    fontFamily: "Times New Roman ",
+                    color: "white",
+                    fontSize: "18px",
+                    paddingX: { xs: "3vh", lg: "0vh" },
+                  }}
+                >
+                  Sit amet, consectetur adipiscing elit.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+          <Box
+            width={"100%"}
+            display={"flex"}
+            justifyContent={{ xs: "center", lg: "flex-start" }}
+            marginBottom={"10vh"}
+            marginTop={"5vh"}
+          >
+            <Button
+              sx={{
+                padding: "2vh 5vh",
+                fontFamily: "Times New Roman ",
+                color: "white",
+              }}
+              variant="contained"
+            >
+              View More
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
---
+++ pages/home/plan_section.tsx
@@ -0,0 +1,16 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+const Plan_section = () => {
+  return (
+    <>
+      <Grid container>
+        <Grid item>
+          <h1>Plan_section</h1>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Plan_section;
